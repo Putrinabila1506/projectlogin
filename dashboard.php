28ad0bbbdf74518cc92f2208f8825f162c442ed3
Putrinabila1506 <watia13peach@gmail.com>
2025-09-26 10:50:59 +0700

    dashboard

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+// cek apakah user sudah login 
+if (!isset($SESSION['username'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <h2>Selamat Datang, <?php echo $_SESSION['username']; ?>!</h2>
+    <p>Role: <?php echo $_SESSION['role']; ?></p>
+    <a href="logout.php">Logout</a>
+</body>
+</html>    
